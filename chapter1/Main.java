23691f2c6c809445bad594def07b7397d0d55f0e
Joaquin Robles <joaquinrobles64@yahoo.com>
2022-01-24 15:41:34 -0800

    Question 1.8 - Zero Matrix

--- chapter1/Main.java
+++ chapter1/Main.java
@@ -1,14 +1,32 @@
 package chapter1;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
 //		System.out.println(Q1_1_IsUnique.solution(""));
+		
 //		System.out.println(Q1_2_CheckPermutation.solution("", ""));
+		
 //		System.out.println(Q1_3_URLify.solution(""));
+		
 //		System.out.println(Q1_4_PalindromePermutation.solution(""));
+		
 //		System.out.println(Q1_5_OneAway.solution("", ""));
+		
 //		System.out.println(Q1_6_StringCompression.solution(""));
+		
+//		int[][] matrix = new int[][] {{1, 2, 3}, {6, 0, 9}, {4, 2, 1}};
+//		System.out.println(Arrays.toString(matrix[0]));
+//		System.out.println(Arrays.toString(matrix[1]));
+//		System.out.println(Arrays.toString(matrix[2]));
+//		System.out.println();
+//		Q1_8_ZeroMatrix.solution(matrix);
+//		System.out.println(Arrays.toString(matrix[0]));
+//		System.out.println(Arrays.toString(matrix[1]));
+//		System.out.println(Arrays.toString(matrix[2]));
+		
 //		System.out.println(Q1_9_StringRotationLeet.solution("", ""));
 	}
 
---
+++ chapter1/Q1_8_ZeroMatrix.java
@@ -0,0 +1,54 @@
+package chapter1;
+
+import java.util.Stack;
+
+public class Q1_8_ZeroMatrix {
+	/* 
+	 * time complexity: O(m*n) 
+	 * space complexity: O(k) where k is the number of 0's in the matrix put into the stack
+	 * */
+	public static void solution(int[][] matrix) {
+		class Tuple {
+			private int[] tuple = new int[2];
+			
+			public Tuple(int val1, int val2) {
+				this.tuple[0] = val1;
+				this.tuple[1] = val2;
+			}
+			
+			private int[] getTuple() {
+				return this.tuple;
+			}
+		}
+		
+		int m = matrix.length;
+		int n = matrix[0].length;
+		Stack<Tuple> tupleStack = new Stack<>();
+		
+		for (int row = 0; row < m; row++) {
+			for (int col = 0; col < n; col++) {
+				if (matrix[row][col] == 0) {
+					tupleStack.push(new Tuple(row, col));
+					break;
+				}
+			}
+		}
+		
+		while (!tupleStack.empty()) {
+			Tuple tuple = tupleStack.pop();
+			int rowToLoop = tuple.getTuple()[0];
+			int colToLoop = tuple.getTuple()[1];
+			
+			// loop through row elements
+			for (int i = 0; i < n; i++) {
+				matrix[rowToLoop][i] = 0;
+			}
+			
+			// loop through col elements
+			for (int i = 0; i < m; i++) {
+				matrix[i][colToLoop] = 0;
+			}
+		}
+	}
+	
+}
