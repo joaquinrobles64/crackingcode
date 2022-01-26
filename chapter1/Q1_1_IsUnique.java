5bf4dea1031ef7923a9176d675c744fb39ec6aa7
Joaquin Robles <joaquinrobles64@yahoo.com>
2022-01-25 18:27:28 -0800

    #1.1: forgot to add time and space complexity

--- chapter1/Q1_1_IsUnique.java
+++ chapter1/Q1_1_IsUnique.java
@@ -3,6 +3,10 @@ package chapter1;
 import java.util.HashSet;
 
 public class Q1_1_IsUnique {
+	/* 
+	 * time complexity: O(n)
+	 * space complexity: O(n)
+	 */
 	static boolean solution(String string) {
 		int n = string.length();
 		HashSet<Character> hashset = new HashSet<>();
